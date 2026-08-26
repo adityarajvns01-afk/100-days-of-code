89cb11071a5d25baff4ad889c6604cdf2649fc2d
adityarajvns01-afk <adityarajvns01@gmail.com>
2026-08-26 23:34:50 +0530

    Update Day1-Q1.c

--- Day1-Q1.c
+++ Day1-Q1.c
@@ -1,18 +1,17 @@
 //
 #include <stdio.h>
-
-int main() {
+int main()
+{
     int num1, num2, sum;
 
-    printf("Enter first number: ");
+    printf("Enter the first number : ");
     scanf("%d", &num1);
 
-    printf("Enter second number: ");
+    printf("Enter the second number : ");
     scanf("%d", &num2);
 
     sum = num1 + num2;
-
-    printf("Sum = %d\n", sum);
-
+    
+    printf("Sum = %d", sum);
     return 0;
 }
